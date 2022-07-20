f028626ab4a76ab5e0a80b0f7394b4baa2620948
olcay <olcay@DESKTOP-K0SB91H>
2022-07-20 20:28:45 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Campİntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("azalış oku");
+            }
+
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("artış oku");
+            }
+            else
+            {
+                Console.WriteLine("değişmedi oku");
+            }
+
+
+            }
+        }
+
+        
+    }
+
